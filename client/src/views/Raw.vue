2a9ab40b40f2ad779f47c561f0ffacd1135697a4
Anibha Gupta <agupta@argusoft.com>
2023-07-15 17:26:22 +0530

    changes done in admindashboard

--- client/src/views/Raw.vue
+++ client/src/views/Raw.vue
@@ -1,54 +1,47 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <!-- Container wrapper -->
-        <div class="container-fluid">
-            <img src="../assets/TimeX.png" alt="" width="70" height="70"> 
-            <h1 > Admil Panel </h1> 
-                <button class="btn btn-outline-success" type="submit">Log Out</button>
-
-            <!-- Toggle button -->
-            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarCenteredExample"
-                aria-controls="navbarCenteredExample" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-
-            <!-- Collapsible wrapper -->
-            <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
-                <!-- Left links -->
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <!-- Navbar dropdown -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-mdb-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <!-- Dropdown menu -->
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                 
-                </ul>
-                <!-- Left links -->
-            </div>
-            <!-- Collapsible wrapper -->
-        </div>
-        <!-- Container wrapper -->
-    </nav>
-</template>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th v-for="(header, index) in tableHeaders" :key="index">{{ header }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, rowIndex) in tableData" :key="rowIndex">
+            <td>{{ rowIndex + 1 }}</td>
+            <td v-for="(cell, columnIndex) in row" :key="columnIndex">
+              <input type="text" v-model="tableData[rowIndex][columnIndex]" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <button @click="updateData">Update</button>
+    </div>
+  </template>
+  <script>
+  export default {
+    data() {
+      return {
+        tableHeaders: ['Column 1', 'Column 2', 'Column 3'], // Replace with your table headers
+        tableData: [ // Replace with your table data
+          ['Row 1 Cell 1', 'Row 1 Cell 2', 'Row 1 Cell 3'],
+          ['Row 2 Cell 1', 'Row 2 Cell 2', 'Row 2 Cell 3'],
+          ['Row 3 Cell 1', 'Row 3 Cell 2', 'Row 3 Cell 3']
+        ]
+      };
+    },
+    methods: {
+      updateData() {
+        // Perform the update logic here
+        const updatedValue = 'New Value'; // Replace with the new value you want to set
+        // Assuming you have the fixed row and column indices
+        const rowIndex = 1; // Replace with your fixed row index
+        const columnIndex = 2; // Replace with your fixed column index
+        // Update the data in the table
+        this.tableData[rowIndex][columnIndex] = updatedValue;
+      }
+    }
+  };
+  </script>
+  
