7e15d07c8d53d5fcaecdb7ea634b0305e981885f
Kanchan Sikder <ksikder@argusoft.com>
2023-11-20 17:27:10 +0530

    bootstrap added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 import routes from "./routes";
 
