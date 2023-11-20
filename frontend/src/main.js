2518528275aa0f8d468f0d94b51447430d313be4
Kanchan Sikder <ksikder@argusoft.com>
2023-11-20 15:50:09 +0530

    added vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import routes from "./routes";
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+  })
+  
+  // 5. Create and mount the root instance.
+  const app = createApp(App)
+  // Make sure to _use_ the router instance to make the
+  // whole app router-aware.
+  app.use(router)
+  
+  app.mount('#app')
---
+++ frontend/src/routes.js
@@ -0,0 +1,52 @@
+// import Register from './views/Register'
+// import AdminDashboard from './views/AdminDashboard'
+// import AdminSettings from './views/AdminSettings'
+// import AboutUs from './views/AboutUs'
+import Home from './views/employeeHome'
+import Login from './views/LoginPage.vue'
+// import Booking from './views/employeeBooking.vue'
+// import Equipments from './views/Equipments'
+
+export default [
+    {
+        path: "/",
+        component: Home,
+        name: "Employee Home",
+    },
+    {
+        path: '/login',
+        component:Login,
+        name: "Login Page"
+    }
+    // {
+    //     path: "/sign/:id",
+    //     component: Register,
+    //     name: "Register",
+    // },
+    // {
+    //     path: "/admin/panel",
+    //     component: AdminDashboard,
+    //     name: "AdminDashboard",
+    // },
+    // {
+    //     path: "/settings",
+    //     component: AdminSettings,
+    //     name: "AdminSettings",
+    // },
+
+    // {
+    //     path: "/about",
+    //     component: AboutUs,
+    //     name: "AboutUs",
+    // },
+    // {
+    //     path:"/employee/booking",
+    //     component:Booking,
+    //     name:'Booking'
+    // },
+    // {
+    //     path: "/employee/equipments",
+    //     component: Equipments,
+    //     name: "equipments",
+    // },
+]
